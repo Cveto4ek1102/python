1cc65db0a635617c56536639db4e915807d4519e
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2021-12-23 17:03:16 +0300

    Delete 3.3.py

--- 3.3.py
+++
@@ -1,9 +0,0 @@
-def my_func(num_1, num_2, num_3):
-    my_list = [num_1, num_2, num_3]
-    try:
-        my_list.remove(min(my_list))
-        return sum(my_list)
-    except TypeError:
-        return "Только число!"
-
-print(my_func(4, -9, 21))
