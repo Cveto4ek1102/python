bd41912a1dc18d0398701d13b99e6b0f82310cb1
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2021-12-27 11:03:55 +0300

    Delete 3.6.py

--- 3.6.py
+++
@@ -1,8 +0,0 @@
-def int_func(text):
-    small_latin = "abcdefghijklmnopqrstuvwxyz"
-    return text.title() if not set(text).difference(small_latin) else False
-words = input("Напишите строку слов, разделенных пробелом: ").split()
-for w in words:
-    result = int_func(w)
-    if result:
-        print(int_func(w).title(), " ")
