0de0e3543976862f7f4d24382a7af0433823add8
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2021-12-30 01:47:05 +0300

    Delete 5.1.py

--- 5.1.py
+++
@@ -1,6 +0,0 @@
-with open('text_1.txt', 'x', encoding='utf-8') as f:
-    while True:
-        line = input('Напишите построчные данные или пустую строку для окончания: ')
-        if not line:
-            break
-        print(line, file=f)
