f3ef9671b9550d384686df3e3ca634b566443f28
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2021-12-30 01:47:44 +0300

    Delete 5.6.py

--- 5.6.py
+++
@@ -1,7 +0,0 @@
-my_list = {}
-with open('text_6.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        les, num = line.split(':')
-        les_sum = sum(map(int, ''.join([i for i in num if i == ' ' or '9' >= i >= '0']).split()))
-        my_list[les] = les_sum
-    print(f'{my_list}')
