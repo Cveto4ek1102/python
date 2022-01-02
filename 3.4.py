cae6afe6bf66f151e082198a4398e4296b642b85
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2022-01-02 21:33:50 +0300

    Delete 3.4.py

--- 3.4.py
+++
@@ -1,7 +0,0 @@
-def my_func(x, y):
-    try:
-        result = x ** y
-    except TypeError:
-        return "Нужно действительное положительное число x и целое отрицательное число y"
-    return result
-print(my_func(8, -3))
