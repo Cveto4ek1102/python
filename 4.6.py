9d7fb2df2fc759773c7de65d35a90d1570b50503
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2022-01-02 21:35:27 +0300

    Delete 4.6.py

--- 4.6.py
+++
@@ -1,6 +0,0 @@
-from itertools import count, cycle
-my_count = count(3)
-my_cycle = cycle("ABC")
-for _ in range(8):
-    print("(my_count, my_cycle) = ({},{})".format(next(my_count), next(my_cycle)))
-
