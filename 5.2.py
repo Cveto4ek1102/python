8cba92d37690500733c4800dc719f9219237f670
Cveto4ek1102 <95653517+Cveto4ek1102@users.noreply.github.com>
2022-01-06 15:51:33 +0300

    Delete 5.2.py

--- 5.2.py
+++
@@ -1,4 +0,0 @@
-with open('text_3.txt', 'r', encoding='utf-8') as f:
-    my_list = [f'{line}. {count.strip()} - {len(count.split())} слов'
-              for line, count in enumerate(f, 1)]
-    print(*my_list, f'Всего строк - {len(my_list)}', sep='\n')
